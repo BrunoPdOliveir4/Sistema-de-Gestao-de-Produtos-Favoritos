2e991ac5f2f8772d0e85a103b865f7399282a972
BrunoPdOliveir4 <bpedrosodroliveira10@gmail.com>
2025-09-13 22:53:49 -0300

    feat: create product requests & service crud

--- eslint.config.cjs
+++ eslint.config.cjs
@@ -2,14 +2,11 @@ module.exports = {
   root: true,
   parser: '@typescript-eslint/parser',
   parserOptions: {
-    project: 'tsconfig.json',
+    project: './tsconfig.eslint.json',
     tsconfigRootDir: __dirname,
     sourceType: 'module',
   },
-  plugins: [
-    '@typescript-eslint/eslint-plugin',
-    '@nestjs/eslint-plugin',
-  ],
+  plugins: ['@typescript-eslint/eslint-plugin', '@nestjs/eslint-plugin'],
   extends: [
     'plugin:@typescript-eslint/recommended',
     'plugin:@nestjs/recommended',
@@ -25,11 +22,6 @@ module.exports = {
     '@typescript-eslint/explicit-function-return-type': 'off',
     '@typescript-eslint/explicit-module-boundary-types': 'off',
     '@typescript-eslint/no-explicit-any': 'off',
-    'prettier/prettier': [
-      'error',
-      {
-        endOfLine: 'auto',
-      },
-    ],
+    'prettier/prettier': ['error', { endOfLine: 'auto' }],
   },
 };
